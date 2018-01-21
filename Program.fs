5e5cc45851454798f6c6405b0248b1aa07bfd6c7
James Gate <james@team16.co.uk>
2018-01-21 16:03:18 +0000

    Started question 17

--- Program.fs
+++ Program.fs
@@ -1,4 +1,5 @@
 ﻿[<EntryPoint>]
 let main argv =
     Question15.answer () |> printfn "Question 15:\n%A\n"
+    Question17.answer () |> printfn "Question 17:\n%A\n"
     0 // return an integer exit code
