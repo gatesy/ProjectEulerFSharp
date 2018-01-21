cdc0ed0fba86d4c54a4825f155970c5b8631d942
James Gate <james@team16.co.uk>
2018-01-21 19:11:55 +0000

    Answer to Q17

--- Question017.fs
+++ Question017.fs
@@ -10,12 +10,44 @@
 // letters and 115 (one hundred and fifteen) contains 20 letters. The use of "and" when writing out 
 // numbers is in compliance with British usage.
 module Question17
-    let nameThatNumber n =
-        match n with
-        | 1 -> "one"
-        | 2 -> "two"
-        | 3 -> "three"
-        | 4 -> "four"
-        | 5 -> "five"
 
-    let answer () = nameThatNumber 3
+let rec nameThatNumber n =
+    match n with
+    | 1 -> "one"
+    | 2 -> "two"
+    | 3 -> "three"
+    | 4 -> "four"
+    | 5 -> "five"
+    | 6 -> "six"
+    | 7 -> "seven"
+    | 8 -> "eight"
+    | 9 -> "nine"
+    | 10 -> "ten"
+    | 11 -> "eleven"
+    | 12 -> "twelve"
+    | 13 -> "thirteen"
+    | 14 -> "fourteen"
+    | 15 -> "fifteen"
+    | 16 -> "sixteen"
+    | 17 -> "seventeen"
+    | 18 -> "eighteen"
+    | 19 -> "nineteen"
+    | 20 -> "twenty"
+    | 30 -> "thirty"
+    | 40 -> "fourty"
+    | 50 -> "fifty"
+    | 60 -> "sixty"
+    | 70 -> "sevety"
+    | 80 -> "eighty"
+    | 90 -> "ninety"
+    | 1000 -> "onethousand"
+    | n when n % 100 = 0 -> nameThatNumber ((n - n % 100) / 100) + "hundred" 
+    | n when n > 20 && n < 100 -> nameThatNumber (n - (n % 10)) + nameThatNumber (n % 10)
+    | n when n > 100 && n < 1000 -> 
+        let remainder = n % 100
+        nameThatNumber (n - n % 100)  + "and" + nameThatNumber remainder
+
+let countLetters fromN toN =
+    seq { fromN .. toN } |> Seq.sumBy (nameThatNumber >> String.length)
+
+let answer () = countLetters 1 1000
