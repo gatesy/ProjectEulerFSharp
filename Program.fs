dba127b2f8efa4028c92afa798ec59257e9a0073
James Gate <james@team16.co.uk>
2018-01-22 00:38:24 +0000

    Added question 19

--- Program.fs
+++ Program.fs
@@ -2,6 +2,7 @@
 let main argv =
     //Question15.answer () |> printfn "Question 15:\n%A\n"
     //Question17.answer () |> printfn "Question 17:\n%A\n"
-    Question18.answer () |> printfn "Question 18:\n%A\n"
-    Question67.answer () |> printfn "Question 67:\n%A\n"
+    //Question18.answer () |> printfn "Question 18:\n%A\n"
+    //Question67.answer () |> printfn "Question 67:\n%A\n"
+    Question19.answer () |> printfn "Question 19:\n%A\n"
     0 // return an integer exit code
