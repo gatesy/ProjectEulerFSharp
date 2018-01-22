42e1bfe680be8b000fab29757ceb6f86d048bd30
James Gate <james@team16.co.uk>
2018-01-22 01:02:22 +0000

    Answered question 20

--- Question020.fs
+++ Question020.fs
@@ -7,4 +7,15 @@
 //
 //Find the sum of the digits in the number 100!
 module Question20
-    let answer () = 1
+
+let factorial n =
+    seq { 1I .. n } |> Seq.fold (*) 1I
+
+let digits number =
+    number.ToString()
+    |> Seq.map ((fun c -> c.ToString()) >> System.Int32.Parse)
+
+let sumOfDigits number =
+    digits number |> Seq.sum
+
+let answer () = factorial 100I |> sumOfDigits
