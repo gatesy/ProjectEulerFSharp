ef01f3a9768107858f73b2639d0a4007192018cf
gatesy <355273+gatesy@users.noreply.github.com>
2018-01-23 23:52:29 +0000

    Answered question 19

--- Question019.fs
+++ Question019.fs
@@ -14,14 +14,14 @@
 // How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
 module Question19
 
-let startDate = System.DateTime.Parse "1998-01-01" 
+let startDate () = System.DateTime.Parse "1901-01-01" 
 
-let endDate = System.DateTime.Parse "2000-12-31"
+let endDate () = System.DateTime.Parse "2000-12-31"
 
 let isLeapYear year =
     year % 400 = 0 || (year % 4 = 0 && year % 100 <> 0)
 
-let daysInMonth year month =
+let daysInMonth (year, month) =
     match month with
     | 1 | 3 | 5 | 7 | 8 | 10 | 12 -> 31
     | 9 | 4 | 6 | 11 -> 30
@@ -29,30 +29,42 @@ let daysInMonth year month =
     | 2 -> 28
     | _ -> invalidArg "month" "valid months are 1-12 (inclusive)"
 
-let daysThatAre1stMonth year =
-    seq { 1 .. 11 } |> Seq.map (daysInMonth year) |> Seq.scan (+) 1
+let daysUntilNext1st (date: System.DateTime) =
+    match date.Day with
+    | 1 -> 0
+    | _ -> daysInMonth (date.Year, date.Month) - date.Day + 1
 
-let firstOfTheMonth (startDate: System.DateTime) (endDate: System.DateTime) =
-    let startingYear = startDate.Year
-    let startingMonth = if startDate.Day = 1 then startDate.Month else startDate.Month + 1
-    let endingYear = endDate.Year
+let firstsOfTheMonth ((startDate: System.DateTime), (endDate: System.DateTime)) =
     let excludeMonths (year,month) =
         match year, month with
-        | startDate.Year, _ when System.DateTime (year, month, 1) < startDate -> true
-        | endingYear, _ when System.DateTime (year, month, 1) > endDate -> true
-        | _ -> false
+        | _, _ when year = startDate.Year && System.DateTime (year, month, 1) < startDate -> false
+        | _, _ when year = endDate.Year && System.DateTime (year, month, 1) > endDate -> false
+        | _ -> true
 
-    [ for year in startingYear .. endingYear do for month in 1 .. 12 -> year,month]
+    [ for year in startDate.Year .. endDate.Year do for month in 1 .. 12 -> year,month]
     |> List.filter excludeMonths
+    |> List.map daysInMonth
+    |> List.scan (+) (daysUntilNext1st startDate)
 
-let rec findNextSunday (date: System.DateTime) =
-    match date.DayOfWeek with
-    | System.DayOfWeek.Sunday -> date
-    | _ -> findNextSunday (date.AddDays 1.0)
+let daysUntilNextSunday (date: System.DateTime) =
+    let rec goThroughDays (fromDate: System.DateTime) dayCount =
+        match fromDate.DayOfWeek with
+        | System.DayOfWeek.Sunday -> dayCount
+        | _ -> goThroughDays (fromDate.AddDays 1.0) dayCount + 1
+    goThroughDays date 0
 
-let sundays startDate endDate =
-    let firstSunday = ((findNextSunday startDate) - startDate)
-    let total = endDate - startDate
-    seq { firstSunday.Days .. 7 .. total.Days}
+let sundays (startDate, endDate) =
+    [ daysUntilNextSunday startDate .. 7 .. (endDate - startDate).Days ]
 
-let answer () = firstOfTheMonth startDate endDate
+let rec intersect sortedList1 sortedList2 =
+    match (sortedList1, sortedList2) with
+    | (x::xs, y::ys) -> 
+        if  x = y then x :: intersect xs ys
+        else if x < y then intersect xs (y::ys) 
+        else intersect (x::xs) ys
+    | [],_ | _,[] -> []
+
+let answer () = 
+    let dateRange = (startDate (), endDate ())
+    intersect (firstsOfTheMonth dateRange) (sundays dateRange)
+    |> List.length
