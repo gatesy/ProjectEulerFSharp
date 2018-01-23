ece26428bcdcf54dcd11ae21ab572bd5ee825d13
James Gate <james@team16.co.uk>
2018-01-23 00:14:12 +0000

    Further attempt at Q19

--- Question019.fs
+++ Question019.fs
@@ -13,4 +13,46 @@
 // by 400.
 // How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
 module Question19
-let answer () = 1
+
+let startDate = System.DateTime.Parse "1998-01-01" 
+
+let endDate = System.DateTime.Parse "2000-12-31"
+
+let isLeapYear year =
+    year % 400 = 0 || (year % 4 = 0 && year % 100 <> 0)
+
+let daysInMonth year month =
+    match month with
+    | 1 | 3 | 5 | 7 | 8 | 10 | 12 -> 31
+    | 9 | 4 | 6 | 11 -> 30
+    | 2 when isLeapYear year -> 29
+    | 2 -> 28
+    | _ -> invalidArg "month" "valid months are 1-12 (inclusive)"
+
+let daysThatAre1stMonth year =
+    seq { 1 .. 11 } |> Seq.map (daysInMonth year) |> Seq.scan (+) 1
+
+let firstOfTheMonth (startDate: System.DateTime) (endDate: System.DateTime) =
+    let startingYear = startDate.Year
+    let startingMonth = if startDate.Day = 1 then startDate.Month else startDate.Month + 1
+    let endingYear = endDate.Year
+    let excludeMonths (year,month) =
+        match year, month with
+        | startDate.Year, _ when System.DateTime (year, month, 1) < startDate -> true
+        | endingYear, _ when System.DateTime (year, month, 1) > endDate -> true
+        | _ -> false
+
+    [ for year in startingYear .. endingYear do for month in 1 .. 12 -> year,month]
+    |> List.filter excludeMonths
+
+let rec findNextSunday (date: System.DateTime) =
+    match date.DayOfWeek with
+    | System.DayOfWeek.Sunday -> date
+    | _ -> findNextSunday (date.AddDays 1.0)
+
+let sundays startDate endDate =
+    let firstSunday = ((findNextSunday startDate) - startDate)
+    let total = endDate - startDate
+    seq { firstSunday.Days .. 7 .. total.Days}
+
+let answer () = firstOfTheMonth startDate endDate
