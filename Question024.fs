5e40ce3cca2226583bf415a99478fd581f5c400b
James Gate <355273+gatesy@users.noreply.github.com>
2018-02-07 00:30:35 +0000

    First attempt

--- Question024.fs
+++ Question024.fs
@@ -10,5 +10,19 @@
 // What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
 
 module Question24
+open Question20
 
-let answer () = 1
+let pickOneItem items =
+    let itemsWithoutI i = items |> List.indexed |> List.choose (fun (j,x) -> if i <> j then Some x else None)
+    items |> List.mapi (fun i el -> (el, itemsWithoutI i))
+ 
+let rec permutations digits =
+    let combine (x,others) = permutations others |> List.map (fun permutation -> x :: permutation)
+
+    match digits with
+    | [] -> []
+    | [x] -> [[x]]
+    | _ -> digits |> pickOneItem |> List.collect combine
+
+let answer () = 
+    permutations [0;1;2;3;4;5;6;7;8;9] |> List.item 1000000
