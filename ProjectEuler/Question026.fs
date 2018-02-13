b862573e0f5b7e8120c532527e69fd55113058a1
James Gate <355273+gatesy@users.noreply.github.com>
2018-02-13 00:18:30 +0000

    Function for converting decimals to fractions

--- ProjectEuler/Question026.fs
+++ ProjectEuler/Question026.fs
@@ -3,15 +3,15 @@
 //
 // A unit fraction contains 1 in the numerator. The decimal representation of the unit fractions with denominators 2 to 10 are given:
 //
-// 1/2	= 	0.5
-// 1/3	= 	0.(3)
-// 1/4	= 	0.25
-// 1/5	= 	0.2
-// 1/6	= 	0.1(6)
-// 1/7	= 	0.(142857)
-// 1/8	= 	0.125
-// 1/9	= 	0.(1)
-// 1/10	= 	0.1
+// 1/2  = 0.5
+// 1/3  = 0.(3)
+// 1/4  = 0.25
+// 1/5  = 0.2
+// 1/6  = 0.1(6)
+// 1/7  = 0.(142857)
+// 1/8  = 0.125
+// 1/9  = 0.(1)
+// 1/10 = 0.1
 // Where 0.1(6) means 0.166666..., and has a 1-digit recurring cycle. It can be seen 
 // that 1/7 has a 6-digit recurring cycle.
 //
@@ -19,4 +19,15 @@
 // its decimal fraction part.
 module Question26
 
-let answer () = 1
+let rec fractionToDecimal numerator denominator =
+    let divides = numerator / denominator
+    let remainder = numerator % denominator
+
+    match (divides,remainder) with
+    | (0,0) -> []
+    | (_,0) -> [divides]
+    | (0,_) -> divides :: fractionToDecimal (numerator * 10) denominator
+    | (_,_) -> divides :: fractionToDecimal (remainder * 10) denominator
+
+let answer () = 
+    [ fractionToDecimal 1 2; fractionToDecimal 2 8; fractionToDecimal 3 100]
